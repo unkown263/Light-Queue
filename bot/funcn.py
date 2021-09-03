9dbc173cc87b77c9dcca4b156e30a85b51debb81
Rick-and-Roll <86619914+Rick-and-Roll@users.noreply.github.com>
2021-09-03 09:34:25 +0100

    Update funcn.py

--- bot/funcn.py
+++ bot/funcn.py
@@ -82,7 +82,7 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         percentage = current * 100 / total
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
-        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+        progress_str = "`➟{0}{1} {2}%` \n\n".format(
             "".join(["🖤" for i in range(math.floor(percentage / 5))]),
             "".join(["🤍" for i in range(10 - math.floor(percentage / 5))]),
             round(percentage, 2),
